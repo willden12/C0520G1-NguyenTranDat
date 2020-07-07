ff10feb9965280aa8afd220aa5410ce94ab99f9b
This PC <nguyentrandat9999@gmail.com>
2020-07-07 08:52:28 +0700

    baitap

---
+++ module2/src/inheritance/thuchanh/TestShape.java
@@ -0,0 +1,11 @@
+package inheritance.thuchanh;
+
+public class TestShape {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }
+}

d4c5f03856d1efa056c776eddd7c20c8e709609e
This PC <nguyentrandat9999@gmail.com>
2020-07-09 09:08:40 +0700

    baitap

--- module2/src/clean_code/baitap/TennisGame.java
+++ module2/src/clean_code/baitap/TennisGame.java
@@ -8,56 +8,56 @@ public class TennisGame {
     public static final int FORTY = 3;
 
     public static String getScore(int play1_Score, int play2_Score) {
-        StringBuilder score = new StringBuilder();
-        int tempScore;
+        StringBuilder point = new StringBuilder();
+        int tempPoint;
         if (play1_Score == play2_Score) {
             switch (play1_Score) {
                 case LOVE:
-                    score = new StringBuilder("Love-All");
+                    point = new StringBuilder("Love-All");
                     break;
                 case FIFTEEN:
-                    score = new StringBuilder("Fifteen-All");
+                    point = new StringBuilder("Fifteen-All");
                     break;
                 case THIRTY:
-                    score = new StringBuilder("Thirty-All");
+                    point = new StringBuilder("Thirty-All");
                     break;
                 case FORTY:
-                    score = new StringBuilder("Forty-All");
+                    point = new StringBuilder("Forty-All");
                     break;
                 default:
-                    score = new StringBuilder("Deuce");
+                    point = new StringBuilder("Deuce");
                     break;
 
             }
         } else if (play1_Score >= 4 || play2_Score >= 4) {
             int minusResult = play1_Score - play2_Score;
-            if (minusResult == 1) score = new StringBuilder("Advantage player1");
-            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
-            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
+            if (minusResult == 1) point = new StringBuilder("Advantage player1");
+            else if (minusResult == -1) point = new StringBuilder("Advantage player2");
+            else if (minusResult >= 2) point = new StringBuilder("Win for player1");
+            else point = new StringBuilder("Win for player2");
         } else {
             for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = play1_Score;
+                if (i == 1) tempPoint = play1_Score;
                 else {
-                    score.append("-");
-                    tempScore = play2_Score;
+                    point.append("-");
+                    tempPoint = play2_Score;
                 }
-                switch (tempScore) {
+                switch (tempPoint) {
                     case LOVE:
-                        score.append("Love");
+                        point.append("Love");
                         break;
                     case FIFTEEN:
-                        score.append("Fifteen");
+                        point.append("Fifteen");
                         break;
                     case THIRTY:
-                        score.append("Thirty");
+                        point.append("Thirty");
                         break;
                     case FORTY:
-                        score.append("Forty");
+                        point.append("Forty");
                         break;
                 }
             }
         }
-        return score.toString();
+        return point.toString();
     }
 }

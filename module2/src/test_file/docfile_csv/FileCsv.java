e925e6c55945d2c36d47d4536b1cd17d69cbcded
This PC <nguyentrandat9999@gmail.com>
2020-07-22 15:42:33 +0700

    baitap

--- module2/src/test_file/docfile_csv/FileCsv.java
+++ module2/src/test_file/docfile_csv/FileCsv.java
@@ -8,21 +8,24 @@ import java.io.IOException;
 public class FileCsv {
     public static void main(String[] args) {
         File file = new File("D:\\CODEGYM\\module2\\src\\test_file\\docfile_csv\\FileCsv");
-        try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-            String[] arrayTemp;
-            while ((line = bufferedReader.readLine()) != null) {
-                arrayTemp = line.split(",");
-                System.out.println("Quốc gia [ Mã: " + arrayTemp[4] + "  Tên: " + arrayTemp[5] + " ]");
+        if (file.isFile()){
+            try {
+                FileReader fileReader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                String line;
+                String[] arrayTemp;
+                while ((line = bufferedReader.readLine()) != null) {
+                    arrayTemp = line.split(",");
+                    System.out.println("Quốc gia [ Mã: " + arrayTemp[4] + "  Tên: " + arrayTemp[5] + " ]");
+                }
+                bufferedReader.close();
+                fileReader.close();
+            }catch (ArrayIndexOutOfBoundsException i){
+                System.out.println("Nhap arrayTemp nho hon 5 nhoe");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            bufferedReader.close();
-            fileReader.close();
-        }catch (ArrayIndexOutOfBoundsException i){
-            System.out.println("Nhap arrayTemp nho hon 5 nhoe");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        }else System.out.println("File ko ton tai");
+
     }
 }
